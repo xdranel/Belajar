604d16dd48ea2a6a37d9050aa50ec8ddf8713614
xdranel <gendhiramona@gmail.com>
2026-03-25 19:53:58 +0700

    Changing some stuff

--- PHP/programerzamannow/project/php-login-management/app/Config/Database.php
+++ PHP/programerzamannow/project/php-login-management/app/Config/Database.php
@@ -4,37 +4,43 @@ namespace Grp\Belajar\Config;
 
 class Database
 {
-    private static ?\PDO $pdo = null;
+    // Store connections in an array so 'test' and 'prod' don't mix up
+    private static array $pdos = [];
+    // Track which one was used last for the transaction methods
+    private static string $lastEnv = "test";
 
     public static function getConnection(string $env = "test"): \PDO
     {
-        if (self::$pdo == null) {
-            // create new PDO
+        self::$lastEnv = $env;
+
+        if (!isset(self::$pdos[$env])) {
             require_once __DIR__ . "/../../config/database.php";
             $config = getDatabaseConfig();
 
-            self::$pdo = new \PDO(
+            // Adding the Error Mode attribute here is highly recommended for MariaDB
+            self::$pdos[$env] = new \PDO(
                 $config['database'][$env]['url'],
                 $config['database'][$env]['username'],
                 $config['database'][$env]['password'],
+                [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
             );
         }
 
-        return self::$pdo;
+        return self::$pdos[$env];
     }
 
     public static function beginTransaction()
     {
-        self::$pdo->beginTransaction();
+        self::$pdos[self::$lastEnv]->beginTransaction();
     }
 
     public static function commitTransaction()
     {
-        self::$pdo->commit();
+        self::$pdos[self::$lastEnv]->commit();
     }
 
     public static function rollbackTransaction()
     {
-        self::$pdo->rollBack();
+        self::$pdos[self::$lastEnv]->rollBack();
     }
 }
--- PHP/programerzamannow/project/php-login-management/config/database.php
+++ PHP/programerzamannow/project/php-login-management/config/database.php
@@ -2,17 +2,26 @@
 
 function getDatabaseConfig(): array
 {
+    $envFile = __DIR__ . '/../.env';
+    if (file_exists($envFile)) {
+        $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue;
+            list($name, $value) = explode('=', $line, 2);
+            putenv(sprintf('%s=%s', trim($name), trim($value)));
+        }
+    }
     return [
         "database" => [
             "test" => [
-                "url" => "mysql:host=localhost;port=3306;dbname=php_login_management_test",
-                "username" => "root",
-                "password" => "plm987!"
+                "url" => "mysql:host=" . getenv('DB_HOST') . ";port=" . getenv('DB_PORT') . ";dbname=" . getenv('DB_NAME_TEST'),
+                "username" => getenv('DB_USER'),
+                "password" => getenv('DB_PASS')
             ],
             "prod" => [
-                "url" => "mysql:host=localhost;port=3306;dbname=php_login_management",
-                "username" => "root",
-                "password" => "plm987!"
+                "url" => "mysql:host=" . getenv('DB_HOST') . ";port=" . getenv('DB_PORT') . ";dbname=" . getenv('DB_NAME_PROD'),
+                "username" => getenv('DB_USER'),
+                "password" => getenv('DB_PASS')
             ]
         ]
     ];
